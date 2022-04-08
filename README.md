c3fd8b01b04f99b925c04670ae9d54571989a31d
Gabriel Simonetto <GabrielSimonetto@users.noreply.github.com>
2022-04-08 18:34:41 -0300

    Create Readme and improve code

---
+++ README.md
@@ -0,0 +1,23 @@
+# bed_to_parquet_conversion
+
+This is demo, to try it out run:
+
+`cargo run samples/well_formed_sample.bed output.parquet`
+
+## Achievables
+
+[x] - Binary that converts a `.bed` with the 3 basic columns {reference_sequence_name, start_position, end_position} to a `.parquet`.
+
+[ ] - Integration Tests:
+    [ ] - Acceptance basic test
+        (troublesome to replicate because of specific parquet.rs objects)
+
+    [ ] - Graciously reject badly formed .bed file
+    [ ] - Graciously reject inexistant .bed file
+
+[ ] - Error messages
+    [ ] - Graciously reject badly formed .bed file
+    [ ] - Graciously reject inexistant .bed file
+
+    [ ] - Make acceptable catch all solution and reporting
+
--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,17 @@ impl Args {
 }
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
-    load_file_convert_to_BedRecord_write_parquet(&args.input_file, &args.output_file);
+    let data = fs::read(&args.input_file)?;
+
+    let mut reader = bed::Reader::new(&data[..]);
+
+    let bed_records_vec = reader
+        .records::<3>()
+        .filter_map(|record| record.ok())
+        .map(convert_noodles_record_to_bedrecord)
+        .collect();
+
+    write_a_records_in_parquet(bed_records_vec, &args.output_file)?;
 
     Ok(())
 }
@@ -43,20 +53,17 @@ use parquet::{
 };
 use parquet_derive::ParquetRecordWriter;
 
+// u64 value chosen according to BED official definition
+// https://samtools.github.io/hts-specs/BEDv1.pdf
 #[derive(ParquetRecordWriter, Debug, PartialEq, Eq)]
 struct BedRecord {
     reference_sequence_name: String,
-    // check if usize is enough
-    start_position: usize,
-    end_position: usize,
+    start_position: u64,
+    end_position: u64,
 }
 
 impl BedRecord {
-    pub fn new(
-        reference_sequence_name: String,
-        start_position: usize,
-        end_position: usize,
-    ) -> Self {
+    pub fn new(reference_sequence_name: String, start_position: u64, end_position: u64) -> Self {
         BedRecord {
             reference_sequence_name,
             start_position,
@@ -65,30 +72,21 @@ impl BedRecord {
     }
 }
 
-fn load_file_convert_to_BedRecord_write_parquet(input_file: &str, output_file: &str) {
-    let data = fs::read(input_file).unwrap();
-
-    let mut reader = bed::Reader::new(&data[..]);
-
-    // how to handle errors?
-    let bed_records_vec: Vec<BedRecord> = reader
-        .records::<3>()
-        .filter_map(|record| record.ok())
-        .map(|record| convert_noodles_record_to_BedRecord(record))
-        .collect();
-
-    write_a_records_in_parquet(bed_records_vec, output_file);
-}
-
-fn convert_noodles_record_to_BedRecord(record: bed::Record<3>) -> BedRecord {
+fn convert_noodles_record_to_bedrecord(record: bed::Record<3>) -> BedRecord {
     BedRecord::new(
         record.reference_sequence_name().to_string(),
-        usize::from(record.start_position()),
-        usize::from(record.end_position()),
+        usize::from(record.start_position()) as u64,
+        usize::from(record.end_position()) as u64,
     )
 }
 
-fn write_a_records_in_parquet(bed_records_vec: Vec<BedRecord>, output_file: &str) {
+fn write_a_records_in_parquet(
+    bed_records_vec: Vec<BedRecord>,
+    output_file: &str,
+) -> Result<(), Box<dyn Error>> {
+    // There is no reference to Unsigned integers on the schema parser
+    // It seems like if we are only using this for storage reasons,
+    // this would have the same behaviour as a unsigned definition
     let message_type = "
   message schema {
       REQUIRED BYTE_ARRAY reference_sequence_name (UTF8);
@@ -99,17 +97,19 @@ fn write_a_records_in_parquet(bed_records_vec: Vec<BedRecord>, output_file: &str
 
     let path = Path::new(output_file);
 
-    let schema = Arc::new(parse_message_type(message_type).unwrap());
+    let schema = Arc::new(parse_message_type(message_type)?);
     let props = Arc::new(WriterProperties::builder().build());
-    let file = fs::File::create(&path).unwrap();
+    let file = fs::File::create(&path)?;
 
-    let mut writer = SerializedFileWriter::new(file, schema, props).unwrap();
-    let mut row_group = writer.next_row_group().unwrap();
+    let mut writer = SerializedFileWriter::new(file, schema, props)?;
+    let mut row_group = writer.next_row_group()?;
 
-    (&bed_records_vec[..]).write_to_row_group(&mut row_group);
+    (&bed_records_vec[..]).write_to_row_group(&mut row_group)?;
 
-    writer.close_row_group(row_group).unwrap();
-    writer.close().unwrap();
+    writer.close_row_group(row_group)?;
+    writer.close()?;
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -119,7 +119,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn converting_to_our_representation() {
+    fn test_converting_noodles_to_our_representation() {
         let record = bed::Record::<3>::builder()
             .set_reference_sequence_name("sq0")
             .set_start_position(Position::try_from(8).unwrap())
@@ -129,7 +129,7 @@ mod tests {
 
         let expected = BedRecord::new("sq0".to_string(), 8, 13);
 
-        let result = convert_noodles_record_to_BedRecord(record);
+        let result = convert_noodles_record_to_bedrecord(record);
 
         assert_eq!(expected, result);
     }
